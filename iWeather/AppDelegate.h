98e93fcbe0da819270856dcc906964b7228c5011
Nguyen Minh Tan <tannguyen@Nguyens-MacBook-Pro-2.local>
2017-03-28 03:12:47 +0700

    Initial Commit

---
+++ iWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWeather
+//
+//  Created by Nguyen Minh Tan on 3/28/17.
+//  Copyright © 2017 Torginization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iWeather
+//
+//  Created by Nguyen Minh Tan on 3/28/17.
+//  Copyright © 2017 Torginization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
